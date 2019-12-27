be6e27c2b5ac3f881893d419d14163d8fd8ec54d
cuong <cuongnm@htactive.com>
2019-12-27 10:17:27 +0700

    make-view-login

--- navigation/Screens.js
+++ navigation/Screens.js
@@ -1,122 +1,145 @@
-import React from 'react';
-import { Easing, Animated, Platform } from 'react-native';
-import { createSwitchNavigator, createStackNavigator, createDrawerNavigator, createAppContainer } from 'react-navigation';
+import React from "react";
+import { Easing, Animated, Platform } from "react-native";
+import {
+  createSwitchNavigator,
+  createStackNavigator,
+  createDrawerNavigator,
+  createAppContainer
+} from "react-navigation";
 
 import { Block, Text, theme } from "galio-framework";
 
-import ComponentsScreen from '../screens/Components';
-import HomeScreen from '../screens/Home';
-import OnboardingScreen from '../screens/Onboarding';
-import ProfileScreen from '../screens/Profile';
-import ProScreen from '../screens/Pro';
-import SettingsScreen from '../screens/Settings';
+import ComponentsScreen from "../screens/Components";
+import HomeScreen from "../screens/Home";
+import OnboardingScreen from "../screens/Onboarding";
+import ProfileScreen from "../screens/Profile";
+import ProScreen from "../screens/Pro";
+import SettingsScreen from "../screens/Settings";
 
-import Menu from './Menu';
-import Header from '../components/Header';
-import { Drawer } from '../components/';
-import LoginScreen from '../screens/Login';
+import Menu from "./Menu";
+import Header from "../components/Header";
+import { Drawer } from "../components/";
+import LoginScreen from "../screens/Login";
 
 const transitionConfig = (transitionProps, prevTransitionProps) => ({
   transitionSpec: {
     duration: 400,
     easing: Easing.out(Easing.poly(4)),
-    timing: Animated.timing,
+    timing: Animated.timing
   },
   screenInterpolator: sceneProps => {
     const { layout, position, scene } = sceneProps;
-    const thisSceneIndex = scene.index
-    const width = layout.initWidth
+    const thisSceneIndex = scene.index;
+    const width = layout.initWidth;
 
     const scale = position.interpolate({
       inputRange: [thisSceneIndex - 1, thisSceneIndex, thisSceneIndex + 1],
       outputRange: [4, 1, 1]
-    })
+    });
     const opacity = position.interpolate({
       inputRange: [thisSceneIndex - 1, thisSceneIndex, thisSceneIndex + 1],
-      outputRange: [0, 1, 1],
-    })
+      outputRange: [0, 1, 1]
+    });
     const translateX = position.interpolate({
       inputRange: [thisSceneIndex - 1, thisSceneIndex],
-      outputRange: [width, 0],
-    })
+      outputRange: [width, 0]
+    });
 
-    const scaleWithOpacity = { opacity }
-    const screenName = "Search"
+    const scaleWithOpacity = { opacity };
+    const screenName = "Search";
 
-    if (screenName === transitionProps.scene.route.routeName ||
-      (prevTransitionProps && screenName === prevTransitionProps.scene.route.routeName)) {
+    if (
+      screenName === transitionProps.scene.route.routeName ||
+      (prevTransitionProps &&
+        screenName === prevTransitionProps.scene.route.routeName)
+    ) {
       return scaleWithOpacity;
     }
-    return { transform: [{ translateX }] }
+    return { transform: [{ translateX }] };
   }
-})
-
-const ProfileStack = createStackNavigator({
-  Profile: {
-    screen: ProfileScreen,
-    navigationOptions: ({ navigation }) => ({
-      header: <Header white transparent title="Profile" navigation={navigation} />,
-      headerTransparent: true,
-    })
-  },
-}, {
-  cardStyle: { backgroundColor: '#EEEEEE', },
-  transitionConfig,
 });
 
-const SettingsStack = createStackNavigator({
-  Settings: {
-    screen: SettingsScreen,
-    navigationOptions: ({ navigation }) => ({
-      header: <Header title="Settings" navigation={navigation} />,
-    })
+const ProfileStack = createStackNavigator(
+  {
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: (
+          <Header white transparent title="Profile" navigation={navigation} />
+        ),
+        headerTransparent: true
+      })
+    }
   },
-}, {
-  cardStyle: { backgroundColor: '#EEEEEE', },
-  transitionConfig,
-});
+  {
+    cardStyle: { backgroundColor: "#EEEEEE" },
+    transitionConfig
+  }
+);
 
-const ComponentsStack = createStackNavigator({
-  Components: {
-    screen: ComponentsScreen,
-    navigationOptions: ({ navigation }) => ({
-      header: <Header title="Components" navigation={navigation} />,
-    })
+const SettingsStack = createStackNavigator(
+  {
+    Settings: {
+      screen: SettingsScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: <Header title="Settings" navigation={navigation} />
+      })
+    }
   },
-}, {
-  cardStyle: { backgroundColor: '#EEEEEE', },
-  transitionConfig,
-});
+  {
+    cardStyle: { backgroundColor: "#EEEEEE" },
+    transitionConfig
+  }
+);
 
-const HomeStack = createStackNavigator({
-  Home: {
-    screen: HomeScreen,
-    navigationOptions: ({ navigation }) => ({
-      header: <Header search tabs title="Home" navigation={navigation} />,
-    })
+const ComponentsStack = createStackNavigator(
+  {
+    Components: {
+      screen: ComponentsScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: <Header title="Components" navigation={navigation} />
+      })
+    }
   },
-  Pro: {
-    screen: ProScreen,
-    navigationOptions: ({ navigation }) => ({
-      header: <Header back white transparent title="" navigation={navigation} />,
-      headerTransparent: true,
-    })
+  {
+    cardStyle: { backgroundColor: "#EEEEEE" },
+    transitionConfig
+  }
+);
+
+const HomeStack = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: <Header search tabs title="Home" navigation={navigation} />
+      })
+    },
+    Pro: {
+      screen: ProScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: (
+          <Header back white transparent title="" navigation={navigation} />
+        ),
+        headerTransparent: true
+      })
+    }
   },
-},
   {
     cardStyle: {
-      backgroundColor: '#EEEEEE', //this is the backgroundColor for the app
+      backgroundColor: "#EEEEEE" //this is the backgroundColor for the app
     },
-    transitionConfig,
-  });
+    transitionConfig
+  }
+);
 
 const AppStack = createDrawerNavigator(
   {
     Onboarding: {
       screen: OnboardingScreen,
       navigationOptions: {
-        drawerLabel: () => { },
-      },
+        drawerLabel: () => {}
+      }
     },
     Home: {
       screen: HomeStack,
@@ -128,88 +151,98 @@ const AppStack = createDrawerNavigator(
     },
     Woman: {
       screen: ProScreen,
-      navigationOptions: (navOpt) => ({
+      navigationOptions: navOpt => ({
         drawerLabel: ({ focused }) => (
           <Drawer focused={focused} screen="Pro" title="Woman" />
-        ),
-      }),
+        )
+      })
     },
     Man: {
       screen: ProScreen,
-      navigationOptions: (navOpt) => ({
+      navigationOptions: navOpt => ({
         drawerLabel: ({ focused }) => (
           <Drawer focused={focused} screen="Pro" title="Man" />
-        ),
-      }),
+        )
+      })
     },
     Kids: {
       screen: ProScreen,
-      navigationOptions: (navOpt) => ({
+      navigationOptions: navOpt => ({
         drawerLabel: ({ focused }) => (
           <Drawer focused={focused} screen="Pro" title="Kids" />
-        ),
-      }),
+        )
+      })
     },
     NewCollection: {
       screen: ProScreen,
-      navigationOptions: (navOpt) => ({
+      navigationOptions: navOpt => ({
         drawerLabel: ({ focused }) => (
           <Drawer focused={focused} screen="Pro" title="New Collection" />
-        ),
-      }),
+        )
+      })
     },
     Profile: {
       screen: ProfileStack,
-      navigationOptions: (navOpt) => ({
+      navigationOptions: navOpt => ({
         drawerLabel: ({ focused }) => (
           <Drawer focused={focused} screen="Profile" title="Profile" />
-        ),
-      }),
+        )
+      })
     },
     Settings: {
       screen: SettingsStack,
-      navigationOptions: (navOpt) => ({
+      navigationOptions: navOpt => ({
         drawerLabel: ({ focused }) => (
           <Drawer focused={focused} screen="Settings" title="Settings" />
-        ),
-      }),
+        )
+      })
     },
     Components: {
       screen: ComponentsStack,
-      navigationOptions: (navOpt) => ({
+      navigationOptions: navOpt => ({
         drawerLabel: ({ focused }) => (
           <Drawer focused={focused} screen="Components" title="Components" />
-        ),
-      }),
+        )
+      })
     },
     MenuDivider: {
       screen: HomeStack,
       navigationOptions: {
-        drawerLabel: () => <Block style={{ marginVertical: 8 }}><Text>{` `}</Text></Block>,
-      },
+        drawerLabel: () => (
+          <Block style={{ marginVertical: 8 }}>
+            <Text>{` `}</Text>
+          </Block>
+        )
+      }
     },
     SignUp: {
       screen: ProScreen,
-      navigationOptions: (navOpt) => ({
+      navigationOptions: navOpt => ({
         drawerLabel: ({ focused }) => (
           <Drawer focused={focused} screen="Pro" title="Sign Up" />
-        ),
-      }),
+        )
+      })
     },
+    LogOut: {
+      screen: LoginScreen,
+      navigationOptions: navOpt => ({
+        drawerLabel: ({ focused }) => (
+          <Drawer focused={focused} screen="Log Out" title="Log Out" />
+        )
+      })
+    }
   },
   Menu
 );
 
-const CheckAuth = createSwitchNavigator(
-  {
-    LoginScreen: {
-      screen: LoginScreen
-    },
-    Auth: {
-      screen: AppStack
-    }
+const CheckAuth = createSwitchNavigator({
+  LoginScreen: {
+    screen: LoginScreen
+  },
+  Auth: {
+    screen: AppStack
   }
-)
+});
 
 const AppContainer = createAppContainer(CheckAuth);
 export default AppContainer;
--- screens/Login.js
+++ screens/Login.js
@@ -1,46 +1,96 @@
-import React from 'react';
-import { ImageBackground, Image, StyleSheet, StatusBar, Dimensions, Platform } from 'react-native';
-import { Block, Button, Text, theme, Input, Icon } from 'galio-framework';
-import { LinearGradient } from 'expo-linear-gradient';
-import { KeyboardAvoidingView } from 'react-native';
+import React from "react";
+import {
+  ImageBackground,
+  Image,
+  StyleSheet,
+  StatusBar,
+  Dimensions,
+  Platform,
+  View
+} from "react-native";
+import {
+  Block,
+  Button,
+  Text,
+  theme,
+  Input,
+  Icon,
+  Checkbox
+} from "galio-framework";
+import { LinearGradient } from "expo-linear-gradient";
+import { KeyboardAvoidingView } from "react-native";
 
-const { height, width } = Dimensions.get('screen');
-import { Images, materialTheme } from '../constants/';
+const { height, width } = Dimensions.get("screen");
+import { Images, materialTheme } from "../constants/";
 import { HeaderHeight } from "../constants/utils";
 
 const LoginScreen = ({ navigation }) => {
-  const iconUsername = <Icon size={16} color={theme.COLORS.MUTED} name="account-circle" family="material" />
-  const iconPassword = <Icon size={16} color={theme.COLORS.MUTED} name="vpn-key" family="material" />
+  const iconUsername = (
+    <Icon
+      size={16}
+      color={theme.COLORS.MUTED}
+      name="account-circle"
+      family="material"
+    />
+  );
+  const iconPassword = (
+    <Icon
+      size={16}
+      color={theme.COLORS.MUTED}
+      name="vpn-key"
+      family="material"
+    />
+  );
   return (
     <Block flex style={styles.container}>
       <StatusBar barStyle="light-content" />
       <Block flex>
         <ImageBackground
-          source={{ uri: Images.Pro }}
+          source={Images.Login}
           style={{ height: height / 1.8, width, zIndex: 1 }}
         >
           <LinearGradient
             style={styles.gradient}
-            colors={['rgba(0,0,0,0)', 'rgba(0,0,0,1)']} />
+            colors={["rgba(0,0,0,0)", "rgba(0,0,0,1)"]}
+          />
         </ImageBackground>
+
         <KeyboardAvoidingView style={styles.padded} behavior="padding" enabled>
+          <Image
+            style={{ height: 200, width: "100%" }}
+            source={Images.Logo}
+            resizeMode="contain"
+          ></Image>
           <Block space="between">
             <Block>
               <Block>
                 <Block>
-                  <Text color="white" size={60}>Đăng nhập</Text>
+                  <Text color="white" size={60}>
+                    Đăng nhập
+                  </Text>
                 </Block>
                 <Block row>
-                  <Text color="white" size={60}>Viet Bill</Text>
+                  <Text color="white" size={60}>
+                    Viet Bill
+                  </Text>
                   <Block middle style={styles.pro}>
-                    <Text size={16} color="white">Mobile</Text>
+                    <Text size={16} color="white">
+                      Mobile
+                    </Text>
                   </Block>
                 </Block>
               </Block>
-              <Text size={16} color='rgba(255,255,255,0.6)'>
-                Phần mềm tính tiền Karaoke, Cafe, Bida, Quán ăn, Nhà Hàng, Shop thời trang, tạp hóa. Cung cấp phần mềm quản lý chuyên nghiệp, miễn phí.
-                </Text>
-              <Block style={{ marginTop: theme.SIZES.BASE * 1.5, marginBottom: theme.SIZES.BASE * 4 }}>
+              <Text size={16} color="rgba(255,255,255,0.6)">
+                Phần mềm tính tiền Karaoke, Cafe, Bida, Quán ăn, Nhà Hàng, Shop
+                thời trang, tạp hóa. Cung cấp phần mềm quản lý chuyên nghiệp,
+                miễn phí.
+              </Text>
+              <Block
+                style={{
+                  marginTop: theme.SIZES.BASE * 1.5,
+                  marginBottom: theme.SIZES.BASE * 1.5
+                }}
+              >
                 <Input
                   left
                   color="black"
@@ -56,39 +106,48 @@ const LoginScreen = ({ navigation }) => {
                   placeholder="Mật khẩu"
                 />
               </Block>
+              <Block style={{ marginBottom: theme.SIZES.BASE * 1.5 }}>
+                <Checkbox
+                  color="warning"
+                  labelStyle={{ color: "#fff" }}
+                  label="Ghi nhớ mật khẩu"
+                />
+              </Block>
               <Button
                 shadowless
                 style={styles.button}
                 color={materialTheme.COLORS.BUTTON_COLOR}
-                onPress={() => navigation.navigate('Home')}>
+                onPress={() => navigation.navigate("Home")}
+              >
                 ĐĂNG NHẬP
-                </Button>
+              </Button>
             </Block>
           </Block>
         </KeyboardAvoidingView>
       </Block>
     </Block>
   );
-}
+};
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.COLORS.BLACK,
-    marginTop: Platform.OS === 'android' ? -HeaderHeight : 0,
+    marginTop: Platform.OS === "android" ? -HeaderHeight : 0
   },
   padded: {
     paddingHorizontal: theme.SIZES.BASE * 2,
     zIndex: 3,
-    position: 'absolute',
-    bottom: Platform.OS === 'android' ? theme.SIZES.BASE * 2 : theme.SIZES.BASE * 3,
+    position: "absolute",
+    bottom:
+      Platform.OS === "android" ? theme.SIZES.BASE * 2 : theme.SIZES.BASE * 3
   },
   button: {
     width: width - theme.SIZES.BASE * 4,
     height: theme.SIZES.BASE * 3,
     shadowRadius: 0,
-    shadowOpacity: 0,
+    shadowOpacity: 0
   },
   pro: {
     backgroundColor: materialTheme.COLORS.LABEL,
@@ -99,10 +158,10 @@ const styles = StyleSheet.create({
   },
   gradient: {
     zIndex: 1,
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
-    height: 66,
-  },
+    height: 66
+  }
 });
