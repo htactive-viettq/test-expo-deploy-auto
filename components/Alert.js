3bcff9e1b8e21069f38bab4600cdd8c9729e506e
cuong <cuongnm@htactive.com>
2019-12-30 17:03:50 +0700

    fix-bug

--- components/Alert.js
+++ components/Alert.js
@@ -1,6 +1,9 @@
-import { Alert } from "react-native";
+import { Alert, Keyboard, Platform } from "react-native";
 
 const PushAlert = props => {
+  {
+    Platform.OS === "android" && Keyboard.dismiss();
+  }
   return Alert.alert("Thông báo!", props, [{ text: "OK" }], {
     cancelable: false
   });
